956e2b20895392111e7fe9840f9dc2272f6726d5
krishna Adhikari <krishna.adhikari@kisio.com>
2019-09-06 16:55:44 +0200

    kirin: format black

--- migrations/versions/21194e8e2308_add_attribute_is_active.py
+++ migrations/versions/21194e8e2308_add_attribute_is_active.py
@@ -8,8 +8,8 @@ Create Date: 2019-09-06 14:56:15.912974
 from __future__ import absolute_import, print_function, unicode_literals, division
 
 # revision identifiers, used by Alembic.
-revision = '21194e8e2308'
-down_revision = u'443587af1780'
+revision = "21194e8e2308"
+down_revision = "443587af1780"
 
 from alembic import op
 import sqlalchemy as sa
@@ -17,9 +17,9 @@ import sqlalchemy as sa
 
 def upgrade():
     # Add column is_active in contributor
-    op.add_column('contributor', sa.Column('is_active', sa.Boolean(), nullable=False, server_default=sa.true()))
+    op.add_column("contributor", sa.Column("is_active", sa.Boolean(), nullable=False, server_default=sa.true()))
 
 
 def downgrade():
     # Drop column is_visible
-    op.drop_column('contributor', 'is_active')
+    op.drop_column("contributor", "is_active")
