e072cc3d676bae67134dcad2619c6f5a89de0fee
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2019-09-26 14:43:40 +0200

    Add tests to check ETag is forgotten or not when managing error

--- tests/integration/gtfs_rt_test.py
+++ tests/integration/gtfs_rt_test.py
@@ -35,13 +35,13 @@ import datetime
 import pytest
 from kirin.core.model import RealTimeUpdate, db, TripUpdate, StopTimeUpdate, VehicleJourney
 from kirin.core.populate_pb import to_posix_time, convert_to_gtfsrt
-from kirin import gtfs_rt
+from kirin import gtfs_rt, redis_client
 from kirin.core.types import TripEffect
 from kirin.tasks import purge_trip_update, purge_rt_update
 from tests import mock_navitia
 from tests.check_utils import dumb_nav_wrapper, api_post
 from kirin import gtfs_realtime_pb2, app
-from kirin.utils import save_rt_data_with_error, manage_db_error
+from kirin.utils import save_rt_data_with_error, manage_db_error, build_redis_etag_key
 from tests.integration.conftest import GTFS_CONTRIBUTOR
 import time
 from sqlalchemy import desc
@@ -125,8 +125,12 @@ def test_wrong_gtfs_rt_post():
     """
     wrong protobuf post on the api
     """
+    redis_client.set(build_redis_etag_key(GTFS_CONTRIBUTOR), "firstETag")  # set ETag key as if it was polled
     res, status = api_post("/gtfs_rt", check=False, data="bob")
 
+    assert (
+        redis_client.get(build_redis_etag_key(GTFS_CONTRIBUTOR)) == "firstETag"
+    )  # error in feed: remember it's processed
     assert status == 400
     assert "invalid protobuf" in res.get("error")
 
@@ -218,8 +222,12 @@ def test_gtfs_rt_simple_delay(basic_gtfs_rt_data, mock_rabbitmq):
 
     after the merge, we should have 4 stops (and only 2 delayed)
     """
+    redis_client.set(build_redis_etag_key(GTFS_CONTRIBUTOR), "firstETag")  # set ETag key as if it was polled
     tester = app.test_client()
     resp = tester.post("/gtfs_rt", data=basic_gtfs_rt_data.SerializeToString())
+    assert (
+        redis_client.get(build_redis_etag_key(GTFS_CONTRIBUTOR)) == "firstETag"
+    )  # all OK: remember it's processed
     assert resp.status_code == 200
 
     with app.app_context():
@@ -1780,6 +1788,7 @@ def test_save_gtfs_rt_with_error():
     test the function "save_gtfs_rt_with_error"
     """
     with app.app_context():
+        redis_client.set(build_redis_etag_key(GTFS_CONTRIBUTOR), "firstETag")  # set ETag key as if it was polled
         save_rt_data_with_error(
             "toto",
             "gtfs-rt",
@@ -1787,6 +1796,9 @@ def test_save_gtfs_rt_with_error():
             error="Decode Error",
             is_reprocess_same_data_allowed=False,
         )
+        assert (
+            redis_client.get(build_redis_etag_key(GTFS_CONTRIBUTOR)) == "firstETag"
+        )  # error in feed: remember it's processed
         assert len(RealTimeUpdate.query.all()) == 1
         assert RealTimeUpdate.query.first().status == "KO"
         assert RealTimeUpdate.query.first().error == "Decode Error"
@@ -1853,6 +1865,7 @@ def test_manage_db_with_http_error_with_insert():
     no gtfs-rt with 'Http Error' inserted since more than 5 seconds
     """
     with app.app_context():
+        redis_client.set(build_redis_etag_key(GTFS_CONTRIBUTOR), "firstETag")  # set ETag key as if it was polled
         manage_db_error(
             "toto",
             "gtfs-rt",
@@ -1860,6 +1873,9 @@ def test_manage_db_with_http_error_with_insert():
             error="Http Error",
             is_reprocess_same_data_allowed=True,
         )
+        assert (
+            redis_client.get(build_redis_etag_key(GTFS_CONTRIBUTOR)) is None
+        )  # external error: forget it was processed and allow reprocess
         assert len(RealTimeUpdate.query.all()) == 1
         assert RealTimeUpdate.query.first().raw_data == "toto"
         assert RealTimeUpdate.query.first().status == "KO"
@@ -1867,6 +1883,9 @@ def test_manage_db_with_http_error_with_insert():
 
         created_at = RealTimeUpdate.query.first().created_at
 
+        redis_client.set(
+            build_redis_etag_key(GTFS_CONTRIBUTOR), "secondETag"
+        )  # set ETag key as if it was polled
         manage_db_error(
             "",
             "gtfs-rt",
@@ -1874,11 +1893,15 @@ def test_manage_db_with_http_error_with_insert():
             error="Decode Error",
             is_reprocess_same_data_allowed=False,
         )
+        assert (
+            redis_client.get(build_redis_etag_key(GTFS_CONTRIBUTOR)) == "secondETag"
+        )  # error in feed: remember it's processed
         assert len(RealTimeUpdate.query.all()) == 2
         assert RealTimeUpdate.query.order_by(desc(RealTimeUpdate.created_at)).first().status == "KO"
         assert RealTimeUpdate.query.order_by(desc(RealTimeUpdate.created_at)).first().error == "Decode Error"
         assert RealTimeUpdate.query.order_by(desc(RealTimeUpdate.created_at)).first().created_at > created_at
 
+        redis_client.set(build_redis_etag_key(GTFS_CONTRIBUTOR), "thirdETag")  # set ETag key as if it was polled
         manage_db_error(
             "toto",
             "gtfs-rt",
@@ -1886,6 +1909,9 @@ def test_manage_db_with_http_error_with_insert():
             error="Http Error",
             is_reprocess_same_data_allowed=True,
         )
+        assert (
+            redis_client.get(build_redis_etag_key(GTFS_CONTRIBUTOR)) is None
+        )  # external error: forget it was processed and allow reprocess
         assert len(RealTimeUpdate.query.all()) == 3
         assert RealTimeUpdate.query.order_by(desc(RealTimeUpdate.created_at)).first().raw_data == "toto"
         assert RealTimeUpdate.query.order_by(desc(RealTimeUpdate.created_at)).first().status == "KO"
