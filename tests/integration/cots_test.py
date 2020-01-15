4e5faf72240c6a0f245170398a6ac9b173ad276c
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2020-01-15 14:41:08 +0100

    Add a full trip reactivation test after full trip removal

--- tests/integration/cots_test.py
+++ tests/integration/cots_test.py
@@ -464,6 +464,50 @@ def test_cots_trip_with_parity(mock_rabbitmq):
     assert mock_rabbitmq.call_count == 1
 
 
+def test_cots_trip_removal_reactivation(mock_rabbitmq):
+    """
+    trip removal, then reactivation
+    """
+    cots_6113 = get_fixture_data("cots_train_6113_trip_removal.json")
+    res = api_post("/cots", data=cots_6113)
+    assert res == "OK"
+
+    with app.app_context():
+        assert len(RealTimeUpdate.query.all()) == 1
+        assert len(TripUpdate.query.all()) == 1
+        assert len(StopTimeUpdate.query.all()) == 0
+    check_db_6113_trip_removal()
+
+    react_6113 = get_fixture_data("cots_train_6113_trip_reactivation.json")
+    res = api_post("/cots", data=react_6113)
+    assert res == "OK"
+
+    with app.app_context():
+        assert len(RealTimeUpdate.query.all()) == 2
+        assert len(TripUpdate.query.all()) == 1
+        assert len(StopTimeUpdate.query.all()) == 4
+
+        db_trip_react = TripUpdate.find_by_dated_vj(
+            "trip:OCETGV-87686006-87751008-2:25768", datetime(2015, 10, 6, 11, 16)
+        )
+        assert db_trip_react
+
+        assert db_trip_react.vj.navitia_trip_id == "trip:OCETGV-87686006-87751008-2:25768"
+        assert db_trip_react.vj.start_timestamp == datetime(2015, 10, 6, 11, 16)
+        assert db_trip_react.vj_id == db_trip_react.vj.id
+        assert db_trip_react.status == "update"
+        assert db_trip_react.effect == "UNKNOWN_EFFECT"
+        assert db_trip_react.message == "Accident à un Passage à Niveau"
+
+        assert len(db_trip_react.stop_time_updates) == 4
+        for s in db_trip_react.stop_time_updates:
+            assert s.arrival_status == "none"
+            assert s.departure_status == "none"
+            assert s.message is None
+
+    assert mock_rabbitmq.call_count == 2
+
+
 def test_cots_trip_with_parity_one_unknown_vj(mock_rabbitmq):
     """
     a trip with a parity has been impacted, but the train 6112 is not known by navitia
