a977d3c04aad22e4e778fc8b3d6bf8ca0e37f156
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2020-10-21 19:57:02 +0200

    PIV: get base VJ from navitia and build corresponding db object VehicleJourney

--- kirin/piv/model_maker.py
+++ kirin/piv/model_maker.py
@@ -31,14 +31,18 @@
 
 from __future__ import absolute_import, print_function, unicode_literals, division
 
-# For perf benches:
-# https://artem.krylysov.com/blog/2015/09/29/benchmark-python-json-libraries/
+import logging
+from datetime import timedelta
+
 import ujson
 from operator import itemgetter
 
+from kirin.core import model
 from kirin.core.abstract_builder import AbstractKirinModelBuilder
-from kirin.exceptions import InvalidArguments, UnauthorizedValue
-from kirin.utils import make_rt_update, get_value
+from kirin.exceptions import InvalidArguments, UnauthorizedValue, ObjectNotFound
+from kirin.utils import make_rt_update, get_value, as_utc_naive_dt, record_internal_failure
+
+TRIP_PIV_ID_FORMAT = "PIV:{}"
 
 
 def _has_departure(stop):
@@ -90,6 +94,34 @@ def _retrieve_interesting_stops(list_ad):
     return res
 
 
+def _is_fully_added_stop(ad):
+    """
+    Check if an arretDesserte object has any event that was present in base-schedule
+    (and return the opposite boolean)
+    """
+    for event_toggle in ["arrivee", "depart"]:
+        event = get_value(ad, event_toggle, nullable=True)
+        if event:
+            if get_value(event, "planTransportSource", nullable=True) not in ["OPE", "PTP"]:
+                return False
+    return True
+
+
+def _get_first_stop_base_datetime(list_ads, hour_obj_name, skip_fully_added_stops=True):
+    if skip_fully_added_stops:
+        s = next((s for s in list_ads if not _is_fully_added_stop(s)), None)
+    else:
+        s = next((s for s in list_ads), None)
+
+    str_time = get_value(get_value(s, hour_obj_name), "dateHeure") if s else None
+    return as_utc_naive_dt(str_time) if str_time else None
+
+
+def _make_navitia_empty_vj(piv_key):
+    trip_id = TRIP_PIV_ID_FORMAT.format(piv_key)
+    return {"id": "vehicle_journey:{}".format(trip_id), "trip": {"id": trip_id}}
+
+
 class KirinModelBuilder(AbstractKirinModelBuilder):
     def __init__(self, contributor):
         super(KirinModelBuilder, self).__init__(contributor, is_new_complete=True)
@@ -134,7 +166,17 @@ class KirinModelBuilder(AbstractKirinModelBuilder):
         elif plan_transport_source and plan_transport_source not in ["PTP", "OPE"]:
             raise UnauthorizedValue("planTransportSource {} is not supported".format(plan_transport_source))
 
+        train_date = get_value(json_train, "dateCirculation")
         train_numbers = get_value(json_train, "numero")
+        train_company = get_value(get_value(json_train, "operateur"), "codeOperateur")
+        mode_dict = get_value(json_train, "modeTransport")
+        train_mode = get_value(mode_dict, "codeMode")
+        train_submode = get_value(mode_dict, "codeSousMode")
+        train_typemode = get_value(mode_dict, "typeMode")
+        piv_key = "{d}:{n}:{c}:{m}:{s}:{t}".format(
+            d=train_date, n=train_numbers, c=train_company, m=train_mode, s=train_submode, t=train_typemode
+        )
+
         list_ads = get_value(json_train, "listeArretsDesserte")
         ads = _retrieve_interesting_stops(get_value(list_ads, "arret"))
         if len(ads) < 2:
@@ -143,8 +185,47 @@ class KirinModelBuilder(AbstractKirinModelBuilder):
                 "json elt {elt}".format(elt=ujson.dumps(json_train))
             )
 
+        vj = self._get_navitia_vj(piv_key, ads)
         # TODO: build trip_update from PIV feed
         trip_updates = []
 
         log_dict = {}
         return trip_updates, log_dict
+
+    def _get_navitia_vj(self, piv_key, ads):
+        log = logging.LoggerAdapter(logging.getLogger(__name__), extra={"contributor": self.contributor.id})
+
+        log.debug("searching for vj {} in navitia".format(piv_key))
+        navitia_vjs = self.navitia.vehicle_journeys(
+            q={
+                "filter": 'vehicle_journey.has_code("rt_piv", "{}")'.format(piv_key),
+                "depth": "2",  # we need this depth to get the stoptime's stop_area
+                "show_codes": "true",  # we need the stop_areas codes
+            }
+        )
+
+        if not navitia_vjs:
+            # Last PIV is always right, so if the VJ doesn't exist, it's an ADD (no matter feed content)
+            navitia_vjs = [_make_navitia_empty_vj(piv_key)]
+
+        vj = None
+        if len(navitia_vjs) != 1:
+            log.info("Can not match a unique train for key {}".format(piv_key))
+            record_internal_failure("no unique train", contributor=self.contributor.id)
+        else:
+            try:
+                base_vs_rt_error_margin = timedelta(hours=1)
+                vj_base_start = _get_first_stop_base_datetime(ads, "depart", skip_fully_added_stops=True)
+                vj = model.VehicleJourney(
+                    navitia_vjs[0],
+                    vj_base_start - base_vs_rt_error_margin,
+                    vj_base_start + base_vs_rt_error_margin,
+                    vj_start_dt=vj_base_start,
+                )
+            except Exception as e:
+                log.exception("Error while creating kirin VJ of {}: {}".format(navitia_vjs[0].get("id"), e))
+                record_internal_failure("Error while creating kirin VJ", contributor=self.contributor.id)
+
+        if not vj:
+            raise ObjectNotFound("no train found for key {}".format(piv_key))
+        return vj
